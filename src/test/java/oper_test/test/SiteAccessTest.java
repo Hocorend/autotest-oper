01e8e73284198ca7a822f71d84fd1b36ce1d9b64
Hocorend <hocorend@gmail.com>
2023-04-08 15:33:19 +0500

    add testng config

--- src/test/java/oper_test/test/SiteAccessTest.java
+++ src/test/java/oper_test/test/SiteAccessTest.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class SiteAccessTest {
+public class SiteAccessTest{
 
     @Test
     @Description("Проверка на доступность сайта")
